757741a3625708e0f1efc09c9138c6dd8253956d
cuu <dexterkidd@gmail.com>
2018-12-06 11:43:18 +0800

    use os/user to find user home dir

--- Menu/GameShell/10_Settings/LauncherPy/plugin_init.go
+++ Menu/GameShell/10_Settings/LauncherPy/plugin_init.go
@@ -2,6 +2,7 @@ package LauncherPy
 
 import (
   "os/exec"
+  "os/user"
   "github.com/cuu/gogame/time"
 	"github.com/cuu/LauncherGoDev/sysgo/UI"
   
@@ -20,8 +21,11 @@ func (self *LauncherPyPlugin) Run( main_screen *UI.MainScreen ) {
     main_screen.MsgBox.SetText("Rebooting to Launcher")
     main_screen.MsgBox.Draw()
 		main_screen.SwapAndShow()
-    time.BlockDelay(300)
-    cmd := exec.Command("sed","-i","s/launchergo/launcher/g","~/.bashrc")
+    time.BlockDelay(550)
+    usr, _ := user.Current()
+    dir := usr.HomeDir
+    
+    cmd := exec.Command("sed","-i","s/launchergo/launcher/g",dir+"/.bashrc")
     cmd.Run()
     
     cmd = exec.Command("sudo","reboot")
