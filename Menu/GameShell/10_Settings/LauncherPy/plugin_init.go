37ae53db278cef37e30b5c999e41850a6fcec889
cpi <cpi@clockworkpi>
2019-05-22 07:45:21 +0000

    minor fix

--- Menu/GameShell/10_Settings/LauncherPy/plugin_init.go
+++ Menu/GameShell/10_Settings/LauncherPy/plugin_init.go
@@ -1,10 +1,11 @@
 package LauncherPy
 
 import (
+  "log"
   "os/exec"
   "os/user"
   "github.com/cuu/gogame/time"
-	"github.com/clockworkpi/LauncherGoDev/sysgo/UI"
+  "github.com/clockworkpi/LauncherGoDev/sysgo/UI"
   
 )
 /******************************************************************************/
@@ -20,16 +21,23 @@ func (self *LauncherPyPlugin) Run( main_screen *UI.MainScreen ) {
 	if main_screen != nil {
     main_screen.MsgBox.SetText("Rebooting to Launcher")
     main_screen.MsgBox.Draw()
-		main_screen.SwapAndShow()
+    main_screen.SwapAndShow()
     time.BlockDelay(550)
     usr, _ := user.Current()
     dir := usr.HomeDir
-    
+    if usr.Username == "root" {
+      dir = "/home/cpi"
+    }
+
     cmd := exec.Command("sed","-i","s/launchergo/launcher/g",dir+"/.bashrc")
-    cmd.Run()
+    err := cmd.Run()
+    if err != nil {
+	log.Println("sed failed", err)
+    }else {
     
     cmd = exec.Command("sudo","reboot")
     cmd.Run()
+    }
 	}
 }
 
