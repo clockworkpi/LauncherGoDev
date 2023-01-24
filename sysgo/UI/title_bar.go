4eefd21a8b6527cf4203aaa5dd40d2c604da0441
cuu <gnubsd@me.com>
2023-01-24 14:47:35 +0800

    Revert "fix title_bar Refresh"
    This reverts commit 961ee5dbb014c70edc05a742bb41dc4bc6be6057.

--- sysgo/UI/title_bar.go
+++ sysgo/UI/title_bar.go
@@ -175,7 +175,6 @@ func (self *TitleBar) UpdateDownloadStatus() {
 
 func (self *TitleBar) RoundRobinCheck() {
 	for {
-		gotime.Sleep(3000 * gotime.Millisecond)
 
 		if self.InLowBackLight < 0 {
 			self.CheckBatteryStat()
@@ -198,6 +197,8 @@ func (self *TitleBar) RoundRobinCheck() {
 
 		}
 
+		gotime.Sleep(3000 * gotime.Millisecond)
+
 	}
 }
 
@@ -217,6 +218,7 @@ func (self *TitleBar) IsWifiConnectedNow() bool {
 }
 
 func (self *TitleBar) UpdateWifiStrength() {
+	self.Draw(self.Title)
 }
 
 func (t *TitleBar) GetWifiStrength() int {
@@ -531,7 +533,7 @@ func (self *TitleBar) Draw(title string) {
 
 	if self.IsWifiConnectedNow() == true {
 		ge := self.GetWifiStrength()
-		fmt.Println("wifi ge: ",ge)
+		//fmt.Println("wifi ge: ",ge)
 		if ge > 0 {
 			self.Icons["wifistatus"].SetIconIndex(ge)
 			self.Icons["wifistatus"].NewCoord(start_x+self.IconWidth+5, self.IconHeight/2+(self.BarHeight-self.IconHeight)/2)
@@ -576,7 +578,6 @@ L:
                 select {
                         case v:= <- self.updateScreen:
                         if v == true {
-				self.Draw(self.Title)
                                 DisplayFlip()
                         }
                         if v== false {
